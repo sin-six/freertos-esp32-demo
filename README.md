7e1748243928b8bec58bf3e12bad86fbd8ac0ae7
sin-six <1052010350@qq.com>
2026-04-08 22:25:55 +0800

    ESP32-FreeRTOS 多传感器采集 AHT20+BMP280 + MQTT局域网上报

--- README.md
+++ README.md
@@ -1,19 +1,21 @@
 # freertos-esp32-demo
 利用rtos实现各种传感器的采集，发送与显示
 
-# ESP32 FreeRTOS + AHT20 温湿度采集 + 本地MQTT(Mosquitto)上报
+# ESP32 FreeRTOS + AHT20 & BMP280 数据采集 + 本地MQTT(Mosquitto)上报
 
 ## 项目简介
 基于 ESP32 + FreeRTOS 实现的多任务温湿度采集系统
 - AHT20 传感器定时采集温湿度
+- BMP280 传感器定时采集气压
 - FreeRTOS 多任务架构（传感器任务 + MQTT任务）
 - 线程安全设计（互斥锁保护串口、I2C总线）
 - 本地私有 Mosquitto MQTT 服务器（数据局域网传输，安全无串扰）
 - MQTT 主动上报数据 + 订阅指令接收
+- 任务优先级设置（传感器任务优先级高于 MQTT 任务）
 
 ## 硬件信息
 - 主控：ESP32
-- 传感器：AHT20 温湿度传感器
+- 传感器：AHT20 温湿度传感器 + BMP280 压力传感器
 - I2C 引脚：
   - SDA：GPIO4
   - SCL：GPIO5
@@ -40,11 +42,11 @@ const char *mqtt_password = "admin";
 ```
 ### 3. 传感器配置
 ```cpp
-#define AHT20_SDA_PIN 4
-#define AHT20_SCL_PIN 5
+#define I2C_SDA_PIN 4
+#define I2C_SCL_PIN 5
 ```
 
 ## 功能说明
 ### 1. 传感器任务
 - 定时采集温湿度数据
-- 采集频率：1s
+- 数据采集频率：1s
--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,9 @@
 #include <stdarg.h>
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include <Adafruit_BMP280.h>
+#include <Wire.h>
+#include <WebServer.h>
 
 #define DEBUG 1
 
@@ -15,6 +18,9 @@
 typedef struct{
   float temperature; // AHT20温度
   float humidity;    // AHT20湿度
+
+  float bmp_temperature; // BMP280温度
+  float bmp_pressure;    // BMP280压力
 } SensorData_t;
 
 // =====================填写你的WIFI =====================
@@ -22,7 +28,7 @@ const char *ssid = "hello world-esp32";
 const char *password = "interesting";
 
 // ===================== MQTT服务器 =====================
-const char *mqtt_server = "192.168.1.100";
+const char *mqtt_server = "192.168.137.1";
 const int mqtt_port = 1883;
 
 //目前没用上
@@ -32,8 +38,13 @@ const char *mqtt_pwd = "esp32_123456";
 const char *mqtt_sub_top = "esp32/cmd";
 const char *mqtt_pub_top = "esp32/aht20";
 
+// ===================== 传感器 =====================
 Adafruit_AHTX0 aht;
 Adafruit_Sensor *aht_humidity, *aht_temp;
+
+Adafruit_BMP280 bmp(&Wire); // use I2C interface
+Adafruit_Sensor *bmp_temp = bmp.getTemperatureSensor();
+Adafruit_Sensor *bmp_pressure = bmp.getPressureSensor();
 
 // ===================== I2C互斥锁 =====================
 SemaphoreHandle_t i2c_mutex; // I2C总线互斥锁
@@ -56,7 +67,7 @@ void safe_printf(const char *format, ...)
  
   xSemaphoreTake(serial_mutex, portMAX_DELAY);
 
-  // 3. 处理可变参数（printf核心）
+  // 处理可变参数（printf核心）
   char buffer[256]; // 打印缓冲区
   va_list args;
   va_start(args, format);
@@ -107,6 +118,8 @@ void mqtt_connect()
   if(client.connected())
     return;
 
+  randomSeed(analogRead(0));
+
   static uint32_t last_retry = 0;
   if(millis() - last_retry < 3000)
     return;
@@ -134,17 +147,12 @@ void sensor_task(void *pvParameters)
   TickType_t last_wake_time;                          // 保存上次唤醒时间
   const TickType_t read_period = pdMS_TO_TICKS(1000); // 周期：1秒
 
-  Wire.begin(I2C_SDA_PIN, I2C_SCL_PIN); // 初始化I2C总线
-
+  // AHT20初始化
   safe_printf("Adafruit AHT10/AHT20 test!\n");
-
-  if (!aht.begin())
+  if (!aht.begin(&Wire))
   {
     safe_printf("Failed to find AHT10/AHT20 chip\n");
-    while (1)
-    {
-      vTaskDelay(pdMS_TO_TICKS(10)); // 延迟10毫秒，避免过多输出
-    }
+    vTaskSuspend(NULL);
   }
 
   safe_printf("AHT10/AHT20 Found!\n");
@@ -158,24 +166,55 @@ void sensor_task(void *pvParameters)
   aht_humidity->printSensorDetails();
 #endif
 
+  // BMP280初始化
+  unsigned status;
+  status = bmp.begin();
+  if(!status){
+    safe_printf("Could not find a valid BMP280 sensor at 0x77, check wiring, address, sensor ID!\n");
+    vTaskDelay(pdMS_TO_TICKS(100));
+    status = bmp.begin(0x76);
+    if(!status){
+      safe_printf("Could not find a valid BMP280 sensor at 0x76, check wiring, address, sensor ID!\n");
+      vTaskSuspend(NULL);
+    }
+  }
+  safe_printf("BMP280 Found!\n");
+
+  /* Default settings from datasheet. */
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+#ifdef DEBUG
+  bmp_temp->printSensorDetails();
+#endif
+  
   last_wake_time = xTaskGetTickCount(); // 初始化：记录系统当前时间
   while(1){
     SensorData_t data; // 传感器数据结构体
 
+    sensors_event_t bmp280_temp_event, bmp280_pressure_event;
     sensors_event_t humidity_event, temp_event;
     xSemaphoreTake(i2c_mutex, portMAX_DELAY);
     aht_humidity->getEvent(&humidity_event);
     aht_temp->getEvent(&temp_event);
+    bmp_temp->getEvent(&bmp280_temp_event);
+    bmp_pressure->getEvent(&bmp280_pressure_event);
     xSemaphoreGive(i2c_mutex);
 
     data.temperature = temp_event.temperature;
     data.humidity = humidity_event.relative_humidity;
+    data.bmp_pressure = bmp280_pressure_event.pressure;
+    data.bmp_temperature = bmp280_temp_event.temperature;
 
     xQueueSend(data_queue, &data, portMAX_DELAY); // 发送数据到队列
 
     // 温湿度格式化打印
     safe_printf("\t\tTemperature %.2f deg C\n", data.temperature);
     safe_printf("\t\tHumidity: %.2f %% rH\n", data.humidity);
+    safe_printf("\t\tBMP280.Temperature = %.2f *C\n", data.bmp_temperature);
+    safe_printf("\t\tBMP280.Pressure = %.2f hPa\n", data.bmp_pressure);
 
     vTaskDelayUntil(&last_wake_time, read_period); // 以固定周期唤醒任务
   }
@@ -185,7 +224,7 @@ void mqtt_task(void *pvParameters)
 {
   client.setServer(mqtt_server, mqtt_port);
   client.setCallback(mqttCallback);
-
+  
   wifi_connect();
 
   while(1){
@@ -197,11 +236,13 @@ void mqtt_task(void *pvParameters)
     if (xQueueReceive(data_queue, &data, pdMS_TO_TICKS(100)) == pdPASS)
     {
       // 温湿度格式化打印
-      safe_printf("\t\tTemperature %.2f deg C\n", data.temperature);
-      safe_printf("\t\tHumidity: %.2f %% rH\n", data.humidity);
+      safe_printf("\t\tmqtt_Temperature %.2f deg C\n", data.temperature);
+      safe_printf("\t\tmqtt_Humidity: %.2f %% rH\n", data.humidity);
+      safe_printf("\t\tmqtt_BMP280.Temperature = %.2f *C\n", data.bmp_temperature);
+      safe_printf("\t\tmqtt_BMP280.Pressure = %.2f hPa\n", data.bmp_pressure);
 
-      char msg[64];
-      snprintf(msg, sizeof(msg), "{\"temperature\": %.2f, \"humidity\": %.2f}", data.temperature, data.humidity);
+      char msg[128];
+      snprintf(msg, sizeof(msg), "{\"temperature\": %.2f, \"humidity\": %.2f, \"bmp_temperature\": %.2f, \"bmp_pressure\": %.2f}", data.temperature, data.humidity, data.bmp_temperature, data.bmp_pressure);
       client.publish(mqtt_pub_top, msg);
       safe_printf("MQTT message published: %s\n", msg);
     }
@@ -211,6 +252,8 @@ void mqtt_task(void *pvParameters)
 void setup() {
   Serial.begin(115200);
   while(!Serial) delay(10);
+
+  Wire.begin(I2C_SDA_PIN, I2C_SCL_PIN); // 初始化I2C总线
 
   // 1. 初始化串口互斥锁
   serial_mutex = xSemaphoreCreateMutex();
@@ -223,12 +266,12 @@ void setup() {
   
   // 2. 创建 sensor 任务
   xTaskCreate(
-      sensor_task,  // 任务函数
+      sensor_task,   // 任务函数
       "Sensor_Task", // 任务名称
-      2048,         // 栈大小
-      NULL,         // 参数
-      9,            // 优先级
-      NULL          // 句柄
+      4096,          // 栈大小
+      NULL,          // 参数
+      9,             // 优先级
+      NULL           // 句柄
   );
 
   xTaskCreate(
@@ -236,7 +279,7 @@ void setup() {
       "MQTT_Task", // 任务名称
       4096,         // 栈大小
       NULL,         // 参数
-      8,            // 优先级
+      10,            // 优先级
       NULL          // 句柄
   );
 
@@ -245,7 +288,7 @@ void setup() {
 }
 
 void loop() {
-  vTaskDelay(pdMS_TO_TICKS(10));
+  vTaskDelay(pdMS_TO_TICKS(1000));
 }
 
 
