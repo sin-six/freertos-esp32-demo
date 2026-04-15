24661842da6199bb196f30f4a5ef71bf183d96fc
sin-six <1052010350@qq.com>
2026-04-15 22:07:26 +0800

    修复睡眠模式:Modem-sleep + DFS + OTA升级固件

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 #include <Wire.h>
 #include "esp_pm.h"
 #include "esp_sleep.h"
-
+#include <ArduinoOTA.h>
 
 #define DEBUG 1
 #define SLEEP_MODE 0 // 0: 不睡眠，1: 轻睡眠
@@ -105,6 +105,10 @@ void wifi_connect()
   {
     safe_printf("\nWiFi connected!\n");
     safe_printf("IP address: %s\n", WiFi.localIP().toString().c_str());
+#if SLEEP_MODE
+    WiFi.setSleep(true);
+    safe_printf("WiFi sleep enabled\n");
+#endif
   }else{
     safe_printf("\nWiFi connect failed!\n");
   }
@@ -152,15 +156,34 @@ void sensor_task(void *pvParameters)
 
   // AHT20初始化
   safe_printf("Adafruit AHT10/AHT20 test!\n");
-  if (!aht.begin(&Wire))
+  uint8_t aht_reset_count = 0;
+  bool aht_state = false;
+  while (aht_reset_count <= 5)
   {
-    safe_printf("Failed to find AHT10/AHT20 chip\n");
-    vTaskSuspend(NULL);
+    xSemaphoreTake(i2c_mutex, portMAX_DELAY);
+    aht_state = aht.begin(&Wire);
+    xSemaphoreGive(i2c_mutex);
+
+    if(aht_state){
+      safe_printf("Found AHT10/AHT20\n");
+      break;
+    }
+
+    safe_printf("Could not find AHT10/AHT20 sensor, check wiring\n");
+    aht_reset_count++;
+    vTaskDelay(pdMS_TO_TICKS(500));
+
   }
 
-  safe_printf("AHT10/AHT20 Found!\n");
+  if (!aht_state)
+  {
+    safe_printf("AHT10/AHT20 initialization failed after multiple attempts!\n");
+    vTaskDelay(pdMS_TO_TICKS(5000));
+    ESP.restart(); // 重启系统，尝试重新初始化传感器
+    //vTaskSuspend(NULL); // 挂起当前任务，停止执行
+  }
 
-  aht_temp = aht.getTemperatureSensor();
+    aht_temp = aht.getTemperatureSensor();
 #if DEBUG
   aht_temp->printSensorDetails();
 #endif
@@ -171,11 +194,16 @@ void sensor_task(void *pvParameters)
 
   // BMP280初始化
   unsigned status;
+  xSemaphoreTake(i2c_mutex, portMAX_DELAY);
   status = bmp.begin();
+  xSemaphoreGive(i2c_mutex);
+
   if(!status){
     safe_printf("Could not find a valid BMP280 sensor at 0x77, check wiring, address, sensor ID!\n");
     vTaskDelay(pdMS_TO_TICKS(100));
+    xSemaphoreTake(i2c_mutex, portMAX_DELAY);
     status = bmp.begin(0x76);
+    xSemaphoreGive(i2c_mutex);
     if(!status){
       safe_printf("Could not find a valid BMP280 sensor at 0x76, check wiring, address, sensor ID!\n");
       vTaskSuspend(NULL);
@@ -223,6 +251,45 @@ void sensor_task(void *pvParameters)
   }
 }
 
+static TickType_t ota_delay = pdMS_TO_TICKS(500);
+void ota_task(void *pvParameters)
+{
+  ArduinoOTA.onStart([]()
+                      { safe_printf("OTA start\n");
+                         ota_delay = pdMS_TO_TICKS(100); // OTA开始后加快循环频率，快速响应OTA事件
+                         WiFi.setSleep(false); // OTA过程中关闭WiFi睡眠，确保连接稳定
+                      });
+
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total)
+                        {
+    static unsigned int last_pct = 0;
+    unsigned int pct = progress / (total / 100);
+    if (pct != last_pct) {
+        last_pct = pct;
+        safe_printf("OTA progress: %u%%\n", pct);
+    } });
+  ArduinoOTA.onEnd([]()
+                   { safe_printf("OTA end\n"); 
+
+                     ota_delay = pdMS_TO_TICKS(500); // OTA结束后恢复循环频率
+                      WiFi.setSleep(true); // OTA结束后重新启用WiFi睡眠
+                  });
+  ArduinoOTA.onError([](ota_error_t error)
+                     { safe_printf("OTA error: %d\n", error);
+                        ota_delay = pdMS_TO_TICKS(500);
+                        WiFi.setSleep(true); // OTA出错后恢复WiFi睡眠
+                    });
+  ArduinoOTA.begin();
+  safe_printf("ArduinoOTA ready\n");
+
+  while (1)
+  {
+    wifi_connect();
+    ArduinoOTA.handle();
+    vTaskDelay(ota_delay);
+  }
+}
+
 void mqtt_task(void *pvParameters)
 {
   client.setServer(mqtt_server, mqtt_port);
@@ -236,7 +303,7 @@ void mqtt_task(void *pvParameters)
     mqtt_connect();
     client.loop();
 
-    if (xQueueReceive(data_queue, &data, pdMS_TO_TICKS(5000)) == pdPASS)
+    if (xQueueReceive(data_queue, &data, pdMS_TO_TICKS(500)) == pdPASS)
     {
       // 温湿度格式化打印
       safe_printf("\t\tmqtt_Temperature %.2f deg C\n", data.temperature);
@@ -259,14 +326,17 @@ void setup() {
 #if SLEEP_MODE 
   esp_pm_config_esp32c3_t pm_config = {
     .max_freq_mhz = 160,
-    .min_freq_mhz = 20,
-    .light_sleep_enable = true
+    .min_freq_mhz = 40,
+    .light_sleep_enable = false // 轻睡眠模式关闭
   };
   if(esp_pm_configure(&pm_config) != ESP_OK ) {
     safe_printf("Failed to configure power management\n");
   }
-#endif 
- 
+  else {
+    safe_printf("Power management configured\n");
+  }
+#endif
+  
 
   Wire.begin(I2C_SDA_PIN, I2C_SCL_PIN); // 初始化I2C总线
 
@@ -277,7 +347,10 @@ void setup() {
   i2c_mutex = xSemaphoreCreateMutex();
 
   // 3. 创建传感器数据队列
-  data_queue = xQueueCreate(5, sizeof(SensorData_t));
+  data_queue = xQueueCreate(10, sizeof(SensorData_t));
+  
+  // 4. 连接WiFi
+  wifi_connect();
   
   // 2. 创建 sensor 任务
   xTaskCreate(
@@ -290,16 +363,21 @@ void setup() {
   );
 
   xTaskCreate(
-      mqtt_task,  // 任务函数
-      "MQTT_Task", // 任务名称
+      mqtt_task,    // 任务函数
+      "MQTT_Task",  // 任务名称
       4096,         // 栈大小
       NULL,         // 参数
       8,            // 优先级
       NULL          // 句柄
   );
 
-
-
+  xTaskCreate(
+      ota_task,   // 任务函数
+      "OTA_Task", // 任务名称
+      4096,       // 栈大小
+      NULL,       // 参数
+      7,          // 优先级
+      NULL);      // 句柄
 }
 
 void loop() {
